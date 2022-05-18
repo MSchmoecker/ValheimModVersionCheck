75639d4b1cdb3be8fbff0bf3d87b27a73d8fce9e
Maximilian Schmöcker <m.schmoecker@brickfire.eu>
2022-05-18 22:53:23 +0200

    fixed DM to bot

--- src/bot.py
+++ src/bot.py
@@ -20,30 +20,35 @@ def run():
     @client.event
     async def on_ready():
         print(f'{client.user} has connected to Discord!')
-        print()
 
     @client.event
     async def on_message(message):
         if message.author == client.user:
             return
 
-        if DEBUG != (message.channel.name == 'vmvc-debug-test'):
+        if DEBUG != (hasattr(message.channel, 'name') and message.channel.name == 'vmvc-debug-test'):
             return
 
         if message.content != "!checkmods":
             return
 
+        print()
+        print("Got request")
+
         if message.reference is None:
+            print("Message has no reference")
             await message.channel.send("Reply to a message containing a logfile")
             return
 
         replied_msg = await message.channel.fetch_message(message.reference.message_id)
 
         if len(replied_msg.attachments) == 0:
+            print("Message has no attachments")
             await message.channel.send("No file attached")
             return
 
         if len(replied_msg.attachments) >= 2:
+            print("Message has too many attachments")
             await message.channel.send("Too many files")
             return
 
@@ -56,9 +61,10 @@ def run():
         modlist.fetch_mods()
         response = compare_mods(mods_local, modlist.mods_online)
 
+        print("Send response with ", len(response), "outdated mods")
+
         if len(response) == 0:
             await message.channel.send("No outdated or old mods found!")
-            print()
             return
 
         tmp = io.StringIO(response)
@@ -66,6 +72,5 @@ def run():
         msg = "Here you go! " \
               "Versions are only read from the logfile and might not match the actual installed version"
         await message.channel.send(msg, file=response_file)
-        print()
 
     client.run(TOKEN)
