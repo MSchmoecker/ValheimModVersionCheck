79a2bff8fe152d471e4cdbbd843add75f27b92f1
Maximilian Schmöcker <m.schmoecker@brickfire.eu>
2022-06-17 19:40:34 +0200

    chore: always print original name of mod to output

--- src/parse.py
+++ src/parse.py
@@ -17,7 +17,6 @@ def parse_local(local_text, is_logfile: bool):
         mod_name = clean_name("".join(line.split(" ")[:-1])).lower()
         mods[mod_name] = {
             "original_name": "".join(line.split(" ")[:-1]),
-            "raw_name": clean_name("".join(line.split(" ")[:-1])),
             "version": "".join(line.split(" ")[-1])
         }
     return mods
@@ -28,18 +27,18 @@ def compare_mods(mods_local, mods_online: Dict[str, Mod]):
     result = ""
 
     for mod in mods_local:
-        raw_name = mods_local[mod]["raw_name"]
+        original_name = mods_local[mod]["original_name"]
         mod_version = mods_local[mod]["version"]
 
         if mod not in mods_online.keys():
-            logging.info(f"{raw_name} not found!")
+            logging.info(f"{original_name} not found!")
             continue
 
         outdated = version.parse(mod_version) < version.parse(mods_online[mod].version)
         old = mods_online[mod].updated < time_threshold
 
         if outdated or old:
-            result += f"{raw_name}\n"
+            result += f"{original_name}\n"
 
         if outdated:
             result += f"\tis outdated {mod_version} -> {mods_online[mod].version}\n"
@@ -49,7 +48,7 @@ def compare_mods(mods_local, mods_online: Dict[str, Mod]):
 
         if version.parse(mod_version) > version.parse(mods_online[mod].version):
             continue
-            result += f"{raw_name} is newer"
+            result += f"{original_name} is newer"
             result += f"\t{mod_version} -> {mods_online[mod].version}\n"
 
     return result
