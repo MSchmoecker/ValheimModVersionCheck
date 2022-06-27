02a70265843e6c7fffe23fd6a409851e5507ea8e
Maximilian Schmöcker <m.schmoecker@brickfire.eu>
2022-06-27 23:20:33 +0200

    feat: handle multiple files in one message

--- src/bot.py
+++ src/bot.py
@@ -8,7 +8,7 @@ import logging
 from readerwriterlock.rwlock import RWLockRead
 from src import ModList, parse_local, compare_mods, fetch_errors, env
 from discord.ext import commands, tasks
-from typing import Optional
+from typing import Optional, List
 
 
 def run(file_lock: RWLockRead):
@@ -39,27 +39,26 @@ def run(file_lock: RWLockRead):
             logging.info("")
             logging.info("Got request !checkmods")
 
-            log = await get_log(message, "!checkmods")
-
-            if log is not None:
-                await on_checkmods(message, message.reference, log, False)
+            logs = await get_logs(message, "!checkmods")
+            if logs is not None:
+                await on_checkmods(message, message.reference, logs, False)
             return
 
         if message.content == "!modlist":
             logging.info("")
             logging.info("Got request !modlist")
 
-            log = await get_log(message, "!modlist")
+            logs = await get_logs(message, "!modlist")
 
-            if log is not None:
-                await on_modlist(message, log)
+            if logs is not None:
+                await on_modlist(message, logs)
             return
 
-        log = await _get_log_from_attachment(message, message.attachments, True)
-        if log is not None and log.splitlines()[0].strip().startswith("[Message:   BepInEx]"):
-            await on_checkmods(message, message, log, True)
+        logs = await _get_logs_from_attachment(message, message.attachments, True)
+        if logs is not None:
+            await on_checkmods(message, message, logs, True)
 
-    async def get_log(message, command_name, silent=False) -> Optional[str]:
+    async def get_logs(message, command_name, silent=False) -> Optional[List[str]]:
         if message.reference is None:
             if not silent:
                 logging.info("Message has no reference")
@@ -67,58 +66,69 @@ def run(file_lock: RWLockRead):
             return None
 
         replied_msg = await message.channel.fetch_message(message.reference.message_id)
-        return await _get_log_from_attachment(message, replied_msg.attachments, silent)
+        return await _get_logs_from_attachment(message, replied_msg.attachments, silent)
 
-    async def _get_log_from_attachment(message, attachments, silent) -> Optional[str]:
+    async def _get_logs_from_attachment(message, attachments, silent) -> Optional[List[str]]:
         if len(attachments) == 0:
             if not silent:
                 logging.info("Message has no attachments")
                 await message.channel.send("No file attached")
             return None
 
-        if len(attachments) >= 2:
-            if not silent:
-                logging.info("Message has too many attachments")
-                await message.channel.send("Too many files")
-            return None
-
         try:
-            attachment_url = attachments[0].url
-            r = requests.get(attachment_url)
-            r.encoding = 'utf-8'
-            return r.text
+            logs = []
+            for attachment in attachments:
+                r = requests.get(attachment.url)
+                r.encoding = 'utf-8'
+                log = r.text
+
+                if log is None or type(log) is not str or len(log) == 0:
+                    continue
+
+                if not log.strip().startswith("[Message:   BepInEx]"):
+                    continue
+
+                logs.append(log)
+            return logs
         except Exception as e:
             logging.exception(f"Failed to get log from attachment: {e}")
-            return None
+            return []
 
-    async def on_checkmods(message, original_message, log, silent_on_no_findings):
-        logging.info("Parse attached file ... ")
-        mods_local = parse_local(log, True)
-        logging.info("done")
-
-        response = compare_mods(mods_local, modlist.get_online_mods())
-        errors = fetch_errors(log)
-
-        if silent_on_no_findings and len(response) == 0:
+    async def on_checkmods(message, original_message, logs, silent_on_no_findings):
+        if not silent_on_no_findings and len(logs) == 0:
+            await message.channel.send("No logs found")
             return
 
-        logging.info(f"Send response with {len(response)} outdated mods and {len(errors)} errors")
+        for log in logs:
+            logging.info("Parse attached file ... ")
+            mods_local = parse_local(log, True)
+            logging.info("done")
 
-        if len(response) == 0 and len(errors) == 0:
-            await message.channel.send("No outdated or old mods found. No errors found.", reference=original_message)
-            return
+            response = compare_mods(mods_local, modlist.get_online_mods())
+            errors = fetch_errors(log)
 
-        response_file_outdated_mods = make_file(response, "mods.txt")
-        response_file_errors = make_file(errors, "errors.txt")
-        response_files = [f for f in [response_file_outdated_mods, response_file_errors] if f is not None]
+            if silent_on_no_findings and len(response) == 0 and len(errors) == 0:
+                return
 
-        msg = "Here you go! " \
-              "A version might not exist if the mod is only available on NexusMods or the name is ambiguous."
-        if len(response) == 0:
-            msg += "No outdated or old mods found. "
-        if len(errors) == 0:
-            msg += "No errors found. "
-        await message.channel.send(msg, files=response_files, reference=original_message)
+            logging.info(
+                f"Send response with {len(response.splitlines())} outdated mods and {len(errors.splitlines())} errors")
+
+            if len(response) == 0 and len(errors) == 0:
+                await message.channel.send("No outdated or old mods found. No errors found.",
+                                           reference=original_message)
+                return
+
+            response_file_outdated_mods = make_file(response, "mods.txt")
+            response_file_errors = make_file(errors, "errors.txt")
+            response_files = [f for f in [response_file_outdated_mods, response_file_errors] if f is not None]
+
+            msg = "Here you go! " \
+                  "A version might not exist if the mod is only available on NexusMods or the name is ambiguous."
+            if len(response) == 0:
+                msg += "No outdated or old mods found. "
+            if len(errors) == 0:
+                msg += "No errors found. "
+            await message.channel.send(msg, files=response_files, reference=original_message)
 
     def make_file(content, filename):
         if content is None or len(content) == 0:
@@ -126,22 +136,21 @@ def run(file_lock: RWLockRead):
         tmp = io.StringIO(content)
         return discord.File(tmp, filename=filename)
 
-    async def on_modlist(message, log):
-        logging.info("Parse attached file ... ")
-        mods_local = parse_local(log, True)
-        logging.info("done")
+    async def on_modlist(message, logs):
+        for log in logs:
+            logging.info("Parse attached file ... ")
+            mods_local = parse_local(log, True)
+            logging.info("done")
 
-        response = ""
+            response = ""
 
-        for mod in sorted(mods_local.values(), key=lambda x: x["original_name"].lower()):
-            response += f'{mod["original_name"]} {mod["version"]}\n'
+            for mod in sorted(mods_local.values(), key=lambda x: x["original_name"].lower()):
+                response += f'{mod["original_name"]} {mod["version"]}\n'
 
-        tmp = io.StringIO(response)
-        response_file = discord.File(tmp, filename="mods.txt")
-        msg = "Here you go!"
-        await message.channel.send(msg, file=response_file)
-
-        modlist.fetch_mods()
+            tmp = io.StringIO(response)
+            response_file = discord.File(tmp, filename="mods.txt")
+            msg = "Here you go!"
+            await message.channel.send(msg, file=response_file)
 
     async def wait_non_blocking(seconds):
         func = functools.partial(time.sleep, seconds)
