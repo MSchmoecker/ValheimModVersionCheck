bb704dc93854f80dad9f9427892f017966322baf
Maximilian Schmöcker <m.schmoecker@brickfire.eu>
2023-09-06 17:00:30 +0200

    chore: moved api to own file

--- app.py
+++ app.py
@@ -1,38 +1,14 @@
 import logging
-import uvicorn
 
-from src import bot, env, schemas, ModList
+from src import bot, ModList, api
 from readerwriterlock import rwlock
-from fastapi import FastAPI
-from fastapi.middleware.gzip import GZipMiddleware
-
-from src.threaded_uvicorn import ThreadedUvicorn
 
 logging.basicConfig(format='[%(asctime)s %(levelname)-8s %(threadName)s] %(message)s', level=logging.INFO,
                     datefmt='%Y-%m-%d %H:%M:%S')
 
-app = FastAPI()
 file_lock = rwlock.RWLockRead()
 modlist: ModList = ModList(file_lock)
 
-app.add_middleware(GZipMiddleware, minimum_size=1000)
-
-
-@app.get("/experimental/thunderstore-mods", response_model=schemas.ModList)
-def thunderstore_mods():
-    return modlist.get_decompiled_mods()
-
-
-@app.get("/experimental/prepared-mods", response_model=schemas.BepInExModList)
-def prepared_mods():
-    """
-        See <a href="https://github.com/MSchmoecker/ValheimModVersionCheck/blob/master/src/modnames.py">Github</a>
-        for the cleanup function used to generate the dictionary keys.
-    """
-
-    return modlist.get_online_mods()
-
-
 if __name__ == '__main__':
-    ThreadedUvicorn(uvicorn.Config(app, host="0.0.0.0", port=env.API_PORT)).start()
+    api.run(modlist)
     bot.run(modlist)
---
+++ src/api.py
@@ -0,0 +1,27 @@
+import uvicorn
+from fastapi import FastAPI
+from starlette.middleware.gzip import GZipMiddleware
+
+from src import ModList, env, schemas
+from src.threaded_uvicorn import ThreadedUvicorn
+
+
+def run(modlist: ModList):
+    app = FastAPI()
+
+    app.add_middleware(GZipMiddleware, minimum_size=1000)
+
+    @app.get("/experimental/thunderstore-mods", response_model=schemas.ModList)
+    def thunderstore_mods():
+        return modlist.get_decompiled_mods()
+
+    @app.get("/experimental/prepared-mods", response_model=schemas.BepInExModList)
+    def prepared_mods():
+        """
+            See <a href="https://github.com/MSchmoecker/ValheimModVersionCheck/blob/master/src/modnames.py">Github</a>
+            for the cleanup function used to generate the dictionary keys.
+        """
+
+        return modlist.get_online_mods()
+
+    ThreadedUvicorn(uvicorn.Config(app, host="0.0.0.0", port=env.API_PORT)).start()
